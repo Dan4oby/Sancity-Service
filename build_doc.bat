2b4d781313251a9340ddf3216061bed2fe890f33
Ученик <@sandsoft.ru>
2026-04-01 12:57:50 +0300

    Убрана кириллица

--- build_doc.bat
+++ build_doc.bat
@@ -4,6 +4,6 @@ del images\*.png
 
 xcopy "..\1C markdown\images" images
 
-pandoc "..\1C markdown\ОбновляторИнформация!.md" -f markdown+wikilinks_title_after_pipe -s -o docs\ОбновляторИнформация.html --self-contained --css=docs\style.css --resource-path=images
+pandoc "..\1C markdown\ОбновляторИнформация!.md" -f markdown+wikilinks_title_after_pipe -s -o docs\download.html --self-contained --css=docs\style.css --resource-path=images
 
-pandoc "..\1C markdown\ОбновляторДемонстрация.md" -f markdown+wikilinks_title_after_pipe -s -o docs\ОбновляторДемонстрация.html --self-contained --resource-path=images 
+pandoc "..\1C markdown\ОбновляторДемонстрация.md" -f markdown+wikilinks_title_after_pipe -s -o docs\index.html --self-contained --resource-path=images 
