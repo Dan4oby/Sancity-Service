fb2f58c300e2781f30029e6675290643ef70d856
Ученик <@sandsoft.ru>
2026-04-02 10:18:22 +0300

    Обновление документации

--- build_doc.bat
+++ build_doc.bat
@@ -6,4 +6,4 @@ xcopy "..\1C markdown\images" images
 
 pandoc "..\1C markdown\sancity-docs\ОбновляторСкачивание!.md" -f markdown+wikilinks_title_after_pipe -s -o docs\download.html --self-contained --css=docs\style.css --resource-path=images
 
-pandoc "..\1C markdown\sancity-docs\Демонстрация.md" -f markdown+wikilinks_title_after_pipe -s -o docs\index.html --self-contained --resource-path=images 
+pandoc "..\1C markdown\sancity-docs\Демонстрация.md" -f markdown+wikilinks_title_after_pipe -s -o docs\index.html --self-contained --css=docs\style.css --resource-path=images 
